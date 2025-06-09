7f6852970684d97ca470c6e546f5027a9106142b
Tirthpatel20 <pateltirth674@gmail.com>
2025-06-09 10:21:44 +0530

    js-delete link not working code

--- javascript-amazon-project-main/scriptss/checkout.js
+++ javascript-amazon-project-main/scriptss/checkout.js
@@ -1,12 +1,11 @@
-import { cart } from "../data/cart.js"
+import {cart,removeFromCart} from "../data/cart.js"
 import { products } from "../data/products.js"
 import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
-cart.forEach((Item, index) => {
+cart.forEach((Item) => {
     const productId = Item.productId
-
     let matchingProduct;
 
     products.forEach((product) => {
@@ -39,7 +38,8 @@ cart.forEach((Item, index) => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="
+                ${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -52,7 +52,7 @@ cart.forEach((Item, index) => {
             <div class="delivery-option">
                 <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-${index}">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Tuesday, June 21
@@ -65,7 +65,7 @@ cart.forEach((Item, index) => {
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-${index}">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Wednesday, June 15
@@ -78,7 +78,7 @@ cart.forEach((Item, index) => {
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-${index}">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Monday, June 13
@@ -95,5 +95,16 @@ cart.forEach((Item, index) => {
 })
 
 
-document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+document.querySelector(".js-order-summary")
+    .innerHTML = cartSummaryHTML;
+
+
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+
+    })
+})
 
